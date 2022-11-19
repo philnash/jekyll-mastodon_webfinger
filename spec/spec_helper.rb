8b77f668d980384175f8dc1a8776c76b8f1821c4
Phil Nash <philnash@gmail.com>
2022-11-19 11:02:39 +0800

    Adds jekyll site fixtures to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,30 @@ end
 
 require "jekyll/mastodon_webfinger"
 
+SOURCE_DIR = File.expand_path("../fixtures", __dir__)
+DEST_DIR = File.expand_path("../dest", __dir__)
+
+def source_dir(*files)
+  File.join(SOURCE_DIR, *files)
+end
+
+def dest_dir(*files)
+  File.join(DEST_DIR, *files)
+end
+
+CONFIG_DEFAULTS = {
+  "source" => source_dir,
+  "destination" => dest_dir,
+  "title" => "Your awesome title",
+  "email" => "your-email@domain.com",
+  "description" => "A description",
+  "baseurl" => "",
+  "url" => "http://yourdomain.com",
+  "twitter_username" => "jekyllrb",
+  "github_username" => "jekyll",
+  "markdown" => "kramdown"
+}.freeze
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -17,5 +41,11 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  def make_site(opts = {})
+    config = Jekyll::Utils.deep_merge_hashes(opts, CONFIG_DEFAULTS)
+    site_config = Jekyll.configuration(config)
+    Jekyll::Site.new(site_config)
   end
 end
