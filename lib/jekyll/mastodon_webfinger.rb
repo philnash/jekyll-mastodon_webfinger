25b685eed37a95265942e51630d73d537a72a8c3
Phil Nash <philnash@gmail.com>
2022-11-25 12:24:28 +1100

    Use site.keep_files directly, rather than via site.config

--- lib/jekyll/mastodon_webfinger.rb
+++ lib/jekyll/mastodon_webfinger.rb
@@ -6,6 +6,6 @@ require_relative "mastodon_webfinger/version"
 require_relative "mastodon_webfinger/generator"
 
 Jekyll::Hooks.register :site, :after_init do |site|
-  keep_files = site.config["keep_files"] || []
-  site.config["keep_files"] = keep_files.push(".well-known/webfinger").uniq
+  keep_files = site.keep_files || []
+  site.keep_files = keep_files.push(".well-known/webfinger").uniq
 end
